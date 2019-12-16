a1d67eda71689df8c8c633c08958b5dd7437d94a
andreloff <andreloff15@hotmail.com>
2019-12-16 13:09:47 -0300

    adicionando screen manager system

---
+++ Screen System/Screen.cs
@@ -0,0 +1,36 @@
+﻿//Screen Management System
+//Developed by Andre Cassio
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Screen : MonoBehaviour
+{
+    public string screenName;
+    //public AudioSource screenAudioSource;
+
+    public virtual void SetScreen()
+    {
+        //Used to prepare screen elements, before show them to player 
+    }
+
+    public virtual void StartScreen()
+    {
+        //Used to start screen elements, like animations
+
+        //screenAudioSource.Play();
+    }
+
+    public virtual void ChangeToNewScreen(Screen _newScreen)
+    {
+        _newScreen.SetScreen();
+        ScreenManager.Instance.currentScreen.gameObject.transform.position = ScreenManager.Instance.cameraOutPos.transform.position;
+        ScreenManager.Instance.currentScreen = _newScreen;
+        ScreenManager.Instance.currentScreen.gameObject.transform.position = ScreenManager.Instance.cameraInPos.transform.position;
+        _newScreen.StartScreen();
+    }
+
+
+}
---
+++ Screen System/ScreenManager.cs
@@ -0,0 +1,42 @@
+﻿//Screen Management System
+//Developed by Andre Cassio
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ScreenManager : Singleton<ScreenManager>
+{
+
+    public Screen startScreen;
+
+    public RectTransform cameraInPos;
+    public RectTransform cameraOutPos;
+
+    //Each individual screen should have its own Manager with singleton
+    public Screen[] screens;
+
+    [HideInInspector]
+    public Screen currentScreen;
+
+    private void Start()
+    {
+        startScreen = screens[0];
+
+        currentScreen = startScreen;
+        currentScreen.SetScreen();
+        currentScreen.transform.position = cameraInPos.transform.position;
+        currentScreen.StartScreen();
+    }
+
+    public void ChangeToScreen(string _screenName)
+    {
+        for(int i = 0; i < screens.Length; i++)
+            if (screens[i].screenName == _screenName) currentScreen.ChangeToNewScreen(screens[i]);
+    }
+    public void ChangeToScreen(int _idx)
+    {
+        currentScreen.ChangeToNewScreen(screens[_idx]);
+    }
+}
